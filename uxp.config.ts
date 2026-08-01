30d75c0d851b9ac9b72b07212c2a0ac5fa82430f
Benson Margulies <bimargulies@gmail.com>
2026-08-01 12:05:47 -0400

    release: v1.1.0

--- uxp.config.ts
+++ uxp.config.ts
@@ -3,7 +3,7 @@ import type { UxpManifest } from '@bubblydoo/vite-uxp-plugin';
 export const manifest: UxpManifest = {
 	id: 'b7e22b3b',
 	name: 'C41 tools',
-	version: '1.0.2',
+	version: '1.1.0',
 	main: 'index.js',
 	manifestVersion: 5,
 	host: [
